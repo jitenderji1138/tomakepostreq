8bf56a357c88d49b61a1615cf4fce620a2a5b751
jitenderji1138 <131745277+jitenderji1138@users.noreply.github.com>
2023-08-07 01:13:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const credentials = require("./credential.json");
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
 })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://free-form.pages.dev');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
 const db = admin.firestore();
